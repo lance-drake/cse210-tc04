405eefa4d8397c8170e751c97edb40a2d91f238a
lance-drake <dra15006@byui.edu>
2021-03-09 16:01:48 -0700

    Updated score

--- hilo/player.py
+++ hilo/player.py
@@ -6,8 +6,8 @@
 class player:
     # Player class that keeps track of a player during each game.
 
-    def __init__(self, score): 
-        self.score=score
+    def __init__(self): 
+        self.score=300
 
 
 
